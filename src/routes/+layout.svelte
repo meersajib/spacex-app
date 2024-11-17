fd4b9927d010f492820b4507d2765d1df0190b2b
Meer Faridul Haque <meer@dirayaah.com>
2024-11-17 19:24:39 +0600

    design the layout of the app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
 	import '../app.css';
+	import { Navbar, NavBrand } from 'flowbite-svelte'
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header class="2xl-container">
+    <Navbar class="h-20 shadow-[0px_1px_3px_0px_#0000001A] flex items-center justify-center p-0">
+        <NavBrand href="/" class="flex items-center justify-center w-full">
+			<img src="/assets/images/logo.png" alt="Logo" />
+        </NavBrand>
+    </Navbar>
+</header>
+
+<main class="pt-[50px] px-[100px]">
+	<div class="flex flex-wrap lg:flex-nowrap gap-[40px]">
+		<!-- Left Column -->
+        <div class="flex-1">
+            <div class="flex items-start justify-between mb-[18px]">
+               left
+            </div>
+        </div>
+        <div class="w-full lg:w-[521px]">
+            <!-- Right column content -->
+            <div class="flex flex-col gap-[26px]">
+                right
+            </div>
+        </div>
+    </div>
+	{@render children()}
+</main>
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,26 @@ import aspectRatio from '@tailwindcss/aspect-ratio';
 import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import flowbitePlugin from 'flowbite/plugin'
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			container: {
+				center: true,
+				screens: {
+				  sm: "640px",
+				  md: "768px",
+				  lg: "1024px",
+				  xl: "1920px",
+				  "2xl": "1920px"
+				},
+			  },
+		}
 	},
 
-	plugins: [typography, forms, containerQueries, aspectRatio]
-} satisfies Config;
+	plugins: [typography, forms, containerQueries, aspectRatio,flowbitePlugin]
+} as Config;
